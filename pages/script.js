0f1585fae070e3d423ea601df0dccd40139d3a4a
Ricardo Ferraz Viana <48634609+ricardofviana@users.noreply.github.com>
2025-09-13 02:07:27 +0000

    Nova página de construção

--- pages/script.js
+++
@@ -1,7 +0,0 @@
-// Create a new image dynamically
-const img = document.createElement("img");
-img.src = "cecilia_ghibli.png"; // reference to your uploaded file
-img.alt = "Artwork from file";
-
-// Append it to the container
-document.getElementById("image-container").appendChild(img);
