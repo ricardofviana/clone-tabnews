c91c81e320bbcaef6e06b8fac217fdaa53182956
Ricardo Viana <ricardo.viana@blip.ai>
2025-10-17 14:38:56 +0800

    makes `npm test` more robust with `async-retry` and `orchestrator.js`

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,10 +1,10 @@
-import database from "infra/database.js"
+import database from "infra/database.js";
+import orcherstrator from "tests/orcherstrator";
 
-beforeAll(cleanDatabase);
-async function cleanDatabase(){
-  await database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public"
-  )
-}
+beforeAll(async () => {
+  await orcherstrator.waitForAllServices();
+  await database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public");
+});
 
 test("GET to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,9 +1,11 @@
 import database from "infra/database.js";
+import orcherstrator from "tests/orcherstrator";
 
-beforeAll(cleanDatabase);
-async function cleanDatabase() {
-  await database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public;");
-}
+beforeAll(async () => {
+  await orcherstrator.waitForAllServices();
+  await database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public");
+});
+
 test("POST to /api/v1/migrations should return 200", async () => {
   const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
