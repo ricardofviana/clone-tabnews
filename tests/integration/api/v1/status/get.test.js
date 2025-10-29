a639ea4a3ad381e68ffdbb4b98e261f831c3678c
Ricardo Viana <ricardo.viana@blip.ai>
2025-10-29 10:10:06 +0800

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,17 +4,21 @@ beforeAll(async () => {
   await orcherstrator.waitForAllServices();
 });
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status ", () => {
+  describe("Anonymous User", () => {
+    test("Retriving current system statu", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
-  expect(responseBody.updated_at).toBeDefined();
+      const responseBody = await response.json();
+      expect(responseBody.updated_at).toBeDefined();
 
-  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  expect(responseBody.dependencies.database.version).toBe("16.0");
-  expect(responseBody.dependencies.database.max_connections).toBe(100);
-  expect(responseBody.dependencies.database.opened_connections).toBe(1);
+      expect(responseBody.dependencies.database.version).toBe("16.0");
+      expect(responseBody.dependencies.database.max_connections).toBe(100);
+      expect(responseBody.dependencies.database.opened_connections).toBe(1);
+    });
+  });
 });
